687be48b7123a079d0d4d25531b4b0f7b8cac6c1
Jason R St John <jasonrstjohn@gmail.com>
2016-08-30 11:49:23 -0400

    trying a default for course

--- catered-to.php
+++ catered-to.php
@@ -233,7 +233,7 @@ function ajax_ct_course(){
    $price = get_post_meta(get_the_ID(),'ct_price' ,true);
    echo '<tr><td>'.$title.'</td><td> $ '.$price.'</td><td><input type="number" style="width:60px"><input type="button" value="Add"></tr>';
 	endwhile; echo '</table>';
-  wp_reset_query();echo '</div>';
+  echo '</div>';
   exit();
 }
 
