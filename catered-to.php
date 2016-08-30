93039a037f3334ef2a57f15e8eb66535c2576a9f
Jason R St John <jasonrstjohn@gmail.com>
2016-08-30 08:40:10 -0400

    trying a default for course

--- catered-to.php
+++ catered-to.php
@@ -247,3 +247,4 @@ add_shortcode( 'ctMenu', 'ajax_ct_course_page' );
 //Add Ajax Actions
 add_action('wp_ajax_course_select_filter', 'ajax_ct_course');
 add_action('wp_ajax_nopriv_course_select_filter', 'ajax_ct_course');
+?>
