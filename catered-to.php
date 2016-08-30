24ede19ccfad3d19d3c45b810f2dcead855e8ba6
Jason R St John <jasonrstjohn@gmail.com>
2016-08-30 08:53:38 -0400

    trying a default for course

--- catered-to.php
+++ catered-to.php
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 function enqueue_ct_ajax_scripts() {
-    wp_register_script( 'ct-ajax-js', 'http://jason.infinity.graphics/steve/wp-content/plugins/catered-to/js/ct.js', array('jquery'));
+    wp_register_script( 'ct-ajax-js', 'http://jason.infinity.graphics/steve/wp-content/plugins/catered-to/js/ct.js', array('jquery'),'0.2', true);
     wp_localize_script( 'ct-ajax-js', 'ajax_genre_params', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
     wp_enqueue_script( 'ct-ajax-js' );
 }
