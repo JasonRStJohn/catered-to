8d2bb824d569cfb5e082e9beb6271d0d49f6fbb1
Jason R St John <jasonrstjohn@gmail.com>
2016-08-30 09:06:46 -0400

    trying a default for course

--- catered-to.php
+++ catered-to.php
@@ -232,7 +232,7 @@ function ajax_ct_course(){
 	 $title = get_the_title();
    $price = get_post_meta(get_the_ID(),'ct_price' ,true);
    echo '<tr><td>'.$title.'</td><td> $ '.$price.'</td><td><input type="number" style="width:60px"><input type="button" value="Add"></tr>';
-	endwhile; echo '</table></div>'; wp_reset_query();
+	endwhile; echo '</table>'; wp_reset_query();
   die();
 }
 
@@ -241,6 +241,7 @@ function ajax_ct_course_page(){
   ajax_ct_course_select();
   echo '<div id="ct-menu">';
   ajax_ct_course();
+  echo '</div>';
 }
 add_shortcode( 'ctMenu', 'ajax_ct_course_page' );
 //Add Ajax Actions
