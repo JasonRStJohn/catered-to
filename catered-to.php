820d6e204e839ea50d5112e1963bf7cbb81e53e9
Jason R St John <jasonrstjohn@gmail.com>
2016-09-01 17:14:57 -0400

    Added Event Custom Post Type and Options Custom Taxonomy for Items

--- catered-to.php
+++ catered-to.php
@@ -32,13 +32,11 @@ function enqueue_ct_ajax_scripts() {
 }
 add_action('wp_enqueue_scripts', 'enqueue_ct_ajax_scripts');
 
-
 //Adds Items and Quotes post types to store individual menu items
-
-
 if ( ! function_exists('ct_item_type') ) {
 
-// Register Custom Post Type
+// Creates Item Post Type
+
 function ct_item_type() {
 
 	$labels = array(
@@ -91,51 +89,68 @@ function ct_item_type() {
 }
 add_action( 'init', 'ct_item_type', 0 );
 }
-//Creates a callback funtion for custom Price metabox in the edit screen of the Items post type
-function custom_meta_box_markup($post)
-{
-   wp_nonce_field(basename(__FILE__), 'ct_price_nonce');
-   $price_value = $post->ct_price;
-   $title_value = $post->post_title;
-   $with_tax_value = $price_value*1.07;
-   echo('$<input type="number" name="ct_price"
-           pattern="[0-9]+([\.,][0-9]+)?" step="0.01" value="'.$price_value.'"
-            title="This should be a number with up to 2 decimal places.">');
-   echo('<br />With tax a '.$title_value.' is $'.round($with_tax_value, 2));
+
+if ( ! function_exists('ct_event') ) {
+
+// Creates Event Post Type
+function ct_event() {
+
+	$labels = array(
+		'name'                  => _x( 'Events', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Event', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Events', 'text_domain' ),
+		'name_admin_bar'        => __( 'Events', 'text_domain' ),
+		'archives'              => __( 'Event Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Events', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Event', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Event', 'text_domain' ),
+		'edit_item'             => __( 'Edit Even', 'text_domain' ),
+		'update_item'           => __( 'Update Event', 'text_domain' ),
+		'view_item'             => __( 'View Event', 'text_domain' ),
+		'search_items'          => __( 'Search Events', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into event', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this event', 'text_domain' ),
+		'items_list'            => __( 'Event list', 'text_domain' ),
+		'items_list_navigation' => __( 'Event list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter events list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Event', 'text_domain' ),
+		'description'           => __( 'Catering Event / Menu', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'ct_event', $args );
+
 }
-//Adds the Price Metabox to Items using the above callback
-function add_custom_meta_box()
-{
-    add_meta_box("demo-meta-box", "Price", "custom_meta_box_markup", "ct_item", "normal", "high", null);
+add_action( 'init', 'ct_event', 0 );
+
 }
 
-add_action("add_meta_boxes", "add_custom_meta_box");
-//Saves Price data to post metadata ct_price
-add_action('save_post', 'save_price_meta');
-function save_price_meta($post_id){
-   $price_input= $_POST['ct_price'];
-   $ct_is_nonce= ( isset( $_POST[ 'ct_price_nonce' ] ) && wp_verify_nonce( $_POST[ 'ct_price_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
-   if ($ct_is_nonce){
-   update_post_meta($post_id,'ct_price',$price_input);
-}
-}
-//Adds Price column to Items list
-add_filter( 'manage_ct_item_posts_columns' , 'add_item_columns');
-function add_item_columns($columns) {
-    return array_merge($columns,
-        array('price'=>__('Price')));
-}
-function custom_ct_item_column($column, $post_id) {
-   switch($column){
-        case 'price':
-            echo('$'.get_post_meta($post_id,'ct_price' ,true));
-            break;
-    }
-}
-add_action('manage_ct_item_posts_custom_column','custom_ct_item_column',10,2);
-// Creates Course Taxonomy
 if ( ! function_exists( 'ct_course_tax' ) ) {
-// Register Custom Taxonomy
+
+// Registers Course Taxonomy
+
 function ct_course_tax() {
 
 	$labels = array(
@@ -175,6 +190,99 @@ function ct_course_tax() {
 add_action( 'init', 'ct_course_tax', 0 );
 
 }
+
+if ( ! function_exists( 'ct_options_tax' ) ) {
+
+//Registers Options Taxonomy
+
+function ct_options_tax() {
+
+	$labels = array(
+		'name'                       => _x( 'Options', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Option', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Options', 'text_domain' ),
+		'all_items'                  => __( 'All Options', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Option Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Option', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Option', 'text_domain' ),
+		'update_item'                => __( 'Update Option', 'text_domain' ),
+		'view_item'                  => __( 'View Option', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Options', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No options', 'text_domain' ),
+		'items_list'                 => __( 'Options List', 'text_domain' ),
+		'items_list_navigation'      => __( 'Options list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'ct_options', array( 'ct_item' ), $args );
+
+}
+add_action( 'init', 'ct_options_tax', 0 );
+
+}
+
+//Creates a callback funtion for custom Price metabox in the edit screen of the Items post type
+function custom_meta_box_markup($post)
+{
+   wp_nonce_field(basename(__FILE__), 'ct_price_nonce');
+   $price_value = $post->ct_price;
+   $title_value = $post->post_title;
+   $with_tax_value = $price_value*1.07;
+   echo('$<input type="number" name="ct_price"
+           pattern="[0-9]+([\.,][0-9]+)?" step="0.01" value="'.$price_value.'"
+            title="This should be a number with up to 2 decimal places.">');
+   echo('<br />With tax a '.$title_value.' is $'.round($with_tax_value, 2));
+}
+//Adds the Price Metabox to Items using the above callback
+
+function add_custom_meta_box() {
+    add_meta_box("demo-meta-box", "Price", "custom_meta_box_markup", "ct_item", "normal", "high", null);
+}
+
+add_action("add_meta_boxes", "add_custom_meta_box");
+
+//Saves Price data to post metadata ct_price
+
+add_action('save_post', 'save_price_meta');
+function save_price_meta($post_id){
+   $price_input= $_POST['ct_price'];
+   $ct_is_nonce= ( isset( $_POST[ 'ct_price_nonce' ] ) && wp_verify_nonce( $_POST[ 'ct_price_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+   if ($ct_is_nonce){
+   update_post_meta($post_id,'ct_price',$price_input);
+}
+}
+
+//Adds Price column to Items list
+
+add_filter( 'manage_ct_item_posts_columns' , 'add_item_columns');
+function add_item_columns($columns) {
+    return array_merge($columns,
+        array('price'=>__('Price')));
+}
+function custom_ct_item_column($column, $post_id) {
+   switch($column){
+        case 'price':
+            echo('$'.get_post_meta($post_id,'ct_price' ,true));
+            break;
+    }
+}
+add_action('manage_ct_item_posts_custom_column','custom_ct_item_column',10,2);
+
+//Creates Course Select Div and Populates Course Catagories
 
 function ajax_ct_course_select(){
     $catloop = get_terms( array('taxonomy'=>'ct_course'));
